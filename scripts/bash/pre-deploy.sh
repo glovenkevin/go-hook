e8f46f03071e22839d8b6c13afd3cfec85cfb64f
kcchandra-w6 <kcchandra@waresix.com>
2025-05-09 00:33:25 +0700

    feat: add ecosystem.config.js creation to pre-deploy script

--- scripts/bash/pre-deploy.sh
+++ scripts/bash/pre-deploy.sh
@@ -5,4 +5,23 @@ echo "Pre-deploy script"
 # create .env file with port 9994
 echo "PORT=9994" > .env
 
+# create ecosystem.config.js file
+echo "module.exports = {
+	apps : [
+	  {
+		name   : "golang-hook",
+		script : "app",
+		interpreter_args: '-u',
+		autorestart: false,
+		instances: 1,
+		env_development: {
+		  NODE_ENV: "development",
+		},
+		env_production: {
+		  NODE_ENV: "production",
+		},
+	  }
+	]
+  }" > ecosystem.config.js
+
 echo "done"
